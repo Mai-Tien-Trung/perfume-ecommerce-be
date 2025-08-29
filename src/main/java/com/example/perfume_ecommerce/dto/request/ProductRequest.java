59f2a372ce030088552e2f900340189ef1e8a675
maitrung0104 <maitrung0104@gmail.com>
2025-08-30 00:22:19 +0700

    Hotfix Product

--- src/main/java/com/example/perfume_ecommerce/dto/request/ProductRequest.java
+++ src/main/java/com/example/perfume_ecommerce/dto/request/ProductRequest.java
@@ -1,16 +1,20 @@
 package com.example.perfume_ecommerce.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class ProductRequest {
+    @NotBlank(message = "Product name is required")
     private String name;
     private String description;
     private String fragranceFamily;
     private String gender;
     private Integer releaseYear;
     private String thumbnailUrl;
+    @NotNull(message = "BrandId is required")
     private Long brandId;
 }
--- src/main/java/com/example/perfume_ecommerce/model/ProductImage.java
+++ src/main/java/com/example/perfume_ecommerce/model/ProductImage.java
@@ -1,5 +1,6 @@
 package com.example.perfume_ecommerce.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -20,5 +21,6 @@ public class ProductImage {
 
     @ManyToOne
     @JoinColumn(name = "product_id", nullable = false)
+    @JsonIgnore
     private Product product;
 }
